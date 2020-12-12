e94716fb814d0bfec297e4e312ca1a58e029b6cd
Kristjan Hollo <hollo.kristjan@gmail.com>
2020-12-12 20:19:35 +0200

    commit to 'main'

---
+++ kool.loop.for.1.py
@@ -0,0 +1,16 @@
+numbrid = list(range(1 , 1_000_001))
+print(min(numbrid))
+print(max(numbrid))
+print(sum(numbrid))
+
+numbrid = list(range(1,21,2))
+print(numbrid)
+
+numbrid = [number*3 for number in range(1,31)]
+print(numbrid)
+
+numbrid = [kuup**3 for kuup in range(1,11)]
+print(numbrid)
+kuubid = []
+kuupid = kuubid.append(numbrid)
+print(kuubid)
