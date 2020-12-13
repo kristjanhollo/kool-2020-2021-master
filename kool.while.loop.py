472a542836dbc4465866ddde914bc6cd6bde25aa
Kristjan Hollo <hollo.kristjan@gmail.com>
2020-12-13 20:42:38 +0200

    main

---
+++ kool.while.loop.py
@@ -0,0 +1,17 @@
+sandwich_orders = ["Beef sandwich", "Pastrami sandwich", "Cheese sandwich", "Pastrami sandwich",
+                   "Chicken sandwich", "Pork sandwich", "Tuna sandwich", "Pastrami sandwich"]
+finished_sandwiches = []
+print("Deli has run out from Pastrami sandwiches! Sorry")
+while "Pastrami sandwich" in sandwich_orders:
+    sandwich_orders.remove("Pastrami sandwich")
+
+while sandwich_orders:
+    sandwich = sandwich_orders.pop()
+
+    print(f"This {sandwich} is ready!")
+    finished_sandwiches.append(sandwich)
+
+print("We are out of sandwiches")
+print(sandwich_orders)
+
+
