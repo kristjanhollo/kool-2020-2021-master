2de454c50b0e5e8d889648a2a1ffa910e1309741
Kristjan Hollo <hollo.kristjan@gmail.com>
2020-12-13 20:55:40 +0200

    main

--- kool.while.loop.py
+++ kool.while.loop.py
@@ -1,7 +1,9 @@
 sandwich_orders = ["Beef sandwich", "Pastrami sandwich", "Cheese sandwich", "Pastrami sandwich",
                    "Chicken sandwich", "Pork sandwich", "Tuna sandwich", "Pastrami sandwich"]
 finished_sandwiches = []
+
 print("Deli has run out from Pastrami sandwiches! Sorry")
+
 while "Pastrami sandwich" in sandwich_orders:
     sandwich_orders.remove("Pastrami sandwich")
 
@@ -13,5 +15,6 @@ while sandwich_orders:
 
 print("We are out of sandwiches")
 print(sandwich_orders)
-
-
+while polling_active:
+    name = input("\nWhat is your name?")
+    favorite_country = input("What is your favorite country"?)
